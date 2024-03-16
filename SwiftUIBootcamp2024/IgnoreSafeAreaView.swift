37979ca48bcbcf4d72bb2948ae2f6a451442847a
Edward <edward@enclave.vn>
2024-03-16 15:44:08 +0700

    #18 Ignore safe area view in swiftUI

---
+++ SwiftUIBootcamp2024/IgnoreSafeAreaView.swift
@@ -0,0 +1,30 @@
+//
+//  IgnoreSafeAreaView.swift
+//  SwiftUIBootcamp2024
+//
+//  Created by Edward Nguyen on 16/03/2024.
+//
+
+import SwiftUI
+
+struct IgnoreSafeAreaView: View {
+    var body: some View {
+        
+        ZStack {
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Hello, Word")
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.blue)
+        }
+    }
+}
+
+#Preview {
+    IgnoreSafeAreaView()
+}
