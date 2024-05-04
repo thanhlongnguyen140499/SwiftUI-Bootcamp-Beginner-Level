82b827d3294cb5dec37d022bf9463e8eb8af6d40
Edward <edward@enclave.vn>
2024-05-04 16:08:25 +0700

    #27 Basic Transitions

---
+++ SwiftUIBootcamp2024/Transition.swift
@@ -0,0 +1,40 @@
+//
+//  Transition.swift
+//  SwiftUIBootcamp2024
+//
+//  Created by Edward Nguyen on 04/05/2024.
+//
+
+import SwiftUI
+
+struct Transition: View {
+    @State var showView: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+//            Color.gray.ignoresSafeArea(.all)
+            
+            VStack {
+                Button("helo") {
+                    withAnimation(.spring(
+                        duration: 0.5, bounce: 0.5, blendDuration: 1
+                    )) {
+                        showView.toggle()
+                    }
+                }
+                Spacer()
+            }
+            
+            if showView {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame( height: UIScreen.main.bounds.height * 0.5)
+                    .transition(.move(edge: .bottom))
+    //                .transition(.slide)
+            }
+        }.edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+#Preview {
+    Transition()
+}
